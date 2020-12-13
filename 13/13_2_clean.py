16e05de2302af7b5def4b4fc2e938b317a167d27
Marvin Vinluan <55716836+k2mv@users.noreply.github.com>
2020-12-13 03:13:42 -0500

    Add files via upload

--- 13/13_2_clean.py
+++ 13/13_2_clean.py
@@ -9,7 +9,7 @@ for n in  range(len(bus_list)):
 		buses.append((int(bus_list[n]), n))
 print(buses)
 
-def do_it3a(original_base, base_magnitude, next_bus, next_bus_offset):
+def do_it3a(base_magnitude, next_bus, next_bus_offset):
 	base = base_magnitude[0]
 	magnitude = base_magnitude[1]
 	done = False
@@ -25,10 +25,9 @@ def do_it3a(original_base, base_magnitude, next_bus, next_bus_offset):
 	print(f'base {base}, magnitude {magnitude}')
 	return (base, magnitude)
 
-original_base = buses[0][0]
 base_magnitude = (buses[0][0], buses[0][0])
 for n in range(1, len(buses)):
-	base_magnitude = do_it3a(original_base, base_magnitude, buses[n][0], buses[n][1])
+	base_magnitude = do_it3a(base_magnitude, buses[n][0], buses[n][1])
 print(base_magnitude)
 
 file_input.close()
